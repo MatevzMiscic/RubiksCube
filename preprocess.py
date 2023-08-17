ba2f27d331d81f43450d492a5b5ef6f3c50ba10d
MatevzMiscic <matevz.miscic@gmail.com>
2023-08-17 13:28:20 +0200

    Added guard before overridin database

--- preprocess.py
+++ preprocess.py
@@ -23,6 +23,7 @@ with open(path, 'br') as file:
 """
 
 
+# BFS
 n = fact[7] * (3 ** 6)
 dist = bytearray(n)
 for i in range(n):
@@ -33,27 +34,28 @@ code = cube.hash()
 queue = deque([(cube, code)])
 dist[code] = 0
 
-
-# BFS
 count = 1
 perc = 36741
 while len(queue) > 0:
     u, c = queue.popleft()
     #print(c, dist[c])
+    count += 1
+    if count % perc == 0:
+        print(count // perc)
     for v in u.neighbours():
         d = v.hash()
-        if d >= n:
-            print(v)
         if dist[d] == 15:
             dist[d] = dist[c] + 1
             queue.append((v, d))
-            count += 1
-            if count % perc == 0:
-                print(count // perc)
 
+
+
+# compress two distances to one byte
 data = bytearray(n // 2)
 for i in range(n // 2):
     data[i] = (dist[2*i] << 4) | dist[2*i + 1]
+
+
 
 
 # save distances to binary file
@@ -61,5 +63,31 @@ folder = os.getcwd()
 filename = "data.bin"
 path = os.path.join(folder, filename)
 
-with open(path, 'bw') as file:
-    file.write(data)
+# getting users permission
+ans = "no"
+while ans not in ["yes", "no"]:
+    print("Do I override to data.bin file? (yes/no)")
+    ans = input()
+if ans == "yes":
+    print("saving data")
+    with open(path, 'bw') as file:
+        file.write(data)
+    print("data saved")
+if ans == "no":
+    print("closing programm")
+
+
+
+
+# test
+def getDist(code):
+    if code % 2 == 0:
+        return data[code // 2] >> 4
+    else:
+        return data[code // 2] & 15
+
+def prneigh(cube):
+    for v in cube.neighbours():
+        print(getDist(v.hash()))
+
+c = Cube2.solved()
