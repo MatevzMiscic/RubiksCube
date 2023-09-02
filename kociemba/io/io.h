d1231f8e654dbe79022b8eeb457199d35391058a
MatevzMiscic <matevz.miscic@gmail.com>
2023-09-02 13:13:25 +0200

    Implemented io for binary files

---
+++ kociemba/io/io.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <string>
+#include "../Settings.h"
+
+
+
+namespace io{
+
+    template<size_t n>
+    bool read(std::string filename, std::array<byte, n>& data){
+        std::ifstream file(filename, std::ios::out | std::ios::binary);
+        if(!file){
+        return false;
+        }
+        file.read((char*)&(data[0]), n);
+        file.close();
+        if(!file.good()){
+        return false;
+        }
+        return true;
+    }
+
+    template<size_t n>
+    int write(std::string filename, std::array<byte, n>& data){
+        std::ofstream file(filename, std::ios::out | std::ios::binary);
+        if(!file){
+            return false;
+        }
+        file.write((char*)&(data[0]), n);
+        file.close();
+        if(!file.good()){
+            return false;
+        }
+        return true;
+    }
+
+}
