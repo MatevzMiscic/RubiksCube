35d50f201192ee28ce1c5803accf74e75849b866
MatevzMiscic <matevz.miscic@gmail.com>
2023-09-07 00:35:45 +0200

    Organising

--- kociemba/math/Combination.h
+++ kociemba/math/Combination.h
@@ -4,29 +4,10 @@
 
 namespace cmb{
 
-    std::array<std::array<short, 14>, 13> bin = {{}};
+    // fills bin with binomial coefficients
+    void calcbin();
 
-    void calcbin(){
-        for(int n = 0; n < 13; ++n){
-            bin[n][0] = 1;
-            for(int k = 1; k < n; ++k){
-                bin[n][k] = bin[n-1][k-1] + bin[n-1][k];
-            }
-            bin[n][n] = 1;
-        }
-    }
-
-    int rank(int n, int k, int comb){
-        int rank = 0;
-        int count = 0;
-        for(int i = 0; i < n; ++i){
-            if(comb & (1 << i)){
-                count += 1;
-                rank += bin[i][count];
-                //printf("i = %d, count = %d\n", i, count);
-            }
-        }
-        return rank;
-    }
+    // computes rank of a combination
+    int rank(int n, int k, int comb);
 
 }
