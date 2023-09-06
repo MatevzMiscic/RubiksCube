4130aeffaa4be78855df304990dc84d20795e63a
MatevzMiscic <matevz.miscic@gmail.com>
2023-09-07 00:35:18 +0200

    Computing symmetries

--- kociemba/Symmetries.h
+++ kociemba/Symmetries.h
@@ -10,7 +10,6 @@ using namespace std;
 namespace sym{
 
     // third turn around an axis through FUR and BDL corners
-    //Cube halfturn(array<byte, 8>{7, 4, 0, 3, 6, 5, 1, 2}, array<byte, 8>{2, 1, 2, 1, 1, 2, 1, 2}, array<byte, 12>{6, 10, 7, 2, 1, 9, 11, 3, 5, 8, 4, 0}, array<byte, 12>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
     Cube thirdturn(std::array<byte, 8>{2, 6, 7, 3, 1, 5, 4, 0}, array<byte, 8>{1, 2, 1, 2, 2, 1, 2, 1}, array<byte, 12>{6, 10, 7, 2, 1, 9, 11, 3, 5, 8, 4, 0}, array<byte, 12>{1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1});
 
     // half turn around an axis that goes through the centers of F and B faces
@@ -21,6 +20,23 @@ namespace sym{
 
     // reflection over the plane that goes throug the middle of the slice between L and R faces
     Cube reflect(array<byte, 8>{1, 0, 3, 2, 5, 4, 7, 6}, array<byte, 8>{3, 3, 3, 3, 3, 3, 3, 3}, array<byte, 12>{0, 3, 2, 1, 5, 4, 7, 6, 8, 11, 10, 9}, array<byte, 12>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+
+    array<Cube, 16> symmetries;
+
+    // fills the symmetries array
+    void compute(){
+        Cube a, b, c;
+        for(int i = 0; i < 2; ++i){
+            for(int j = 0; j < 4; ++j){
+                for(int k = 0; k < 2; ++k){
+                    symmetries[8*i + 2*j + k] = a * b * c;
+                    c *= reflect;
+                }
+                b *= quarturn;
+            }
+            a *= halfturn;
+        }
+    }
 
     void applyHalfturn2(Cube& cube){
         Cube sym(array<byte, 8>{5, 4, 7, 6, 1, 0, 3, 2}, array<byte, 8>{0, 0, 0, 0, 0, 0, 0, 0}, array<byte, 12>{8, 11, 10, 9, 5, 4, 7, 6, 0, 3, 2, 1}, array<byte, 12>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
@@ -50,4 +66,5 @@ namespace sym{
     void applyReflect(Cube& cube){
         cube = reflect.inv() * cube * reflect;
     }
+
 }
