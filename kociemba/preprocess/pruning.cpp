c57f8972bcc1959fcf443268cc38a26400514763
MatevzMiscic <matevz.miscic@gmail.com>
2023-09-15 10:23:14 +0200

    Code for calculation pruning tables

--- kociemba/preprocess/pruning.cpp
+++ kociemba/preprocess/pruning.cpp
@@ -26,7 +26,6 @@ struct coord{
     }
     coord(Cube& cube){
         this->val = (cube.flip_coord() << 21) | (cube.twist_coord() << 9) | cube.slice_coord();
-
     }
 
     inline int slice(){
@@ -52,7 +51,16 @@ struct coord{
     inline coord neigh(int i){
         return coord(slicemt[18*slice() + i], twistmt[18*twist() + i], flipmt[18*flip() + i]);
     }
+
+    void print(){
+        printf("%d, idx: %d, slice: %d, twist: %d, flip: %d\n", val, index(), slice(), flip(), twist());
+    }
 };
+
+std::array<short, 18*2187> coord::twistmt;
+std::array<short, 18*2048> coord::flipmt;
+std::array<short, 18*495> coord::slicemt;
+
 
 
 
@@ -71,10 +79,10 @@ void printb(int n){
 struct table{
     // table of length len, where all each entry has two bits
 
-    int len;
+    ll len;
     int* bits;
     
-    table(int n){
+    table(ll n){
         len = n;
         bits = new int[n/16 + 1];
     }
@@ -137,25 +145,89 @@ void pruning_table(table& arr){
         queue.pop_front();
         int index_u = u.index();
         int dist = arr.get(index_u) + 1;
+        if(dist == 3) dist = 0;
         if(distance % 3 != dist){
             distance += 1;
-            if(distance == 3) break;
+            //printf("dist: %d, distance: %d\n", dist, distance);
+            if(distance == 4) break;
         }
+        printf("u = %d, dist = %d\n", index_u, distance-1);
         for(int i = 0; i < 18; ++i){
             coord v = u.neigh(i);
             int index = v.index();
             if(arr.get(index) == 3){
-                if(dist == 3) dist = 0;
+                printf("  node %d found\n", index);
                 arr.set(index, dist);
                 queue.push_back(v);
             }
         }
     }
-    printf("count = %d\n", count);
+    printf("count = %d\n", count-1);
 }
 
 
+int idx(Cube& cube){
+    return 2187 * ((cube.slice_coord() << 11) | cube.flip_coord()) + cube.twist_coord();
+}
+
+Cube neigh(Cube& c, int i){
+    return c * Cube::moves[i];
+}
+
+void pruning_table2(table& arr){
+    arr.fill(3);
+    printf("Table filled.\n");
+
+    int distance = 0;
+    Cube id;
+    deque<Cube> queue = {id};
+    int index = idx(id);
+    arr.set(index, 0);
+
+    int count = 0;
+
+    while(!queue.empty()){
+        count += 1;
+        Cube u = queue.front();
+        queue.pop_front();
+        int index_u = idx(u);
+        int dist = arr.get(index_u) + 1;
+        if(dist == 3) dist = 0;
+        if(distance % 3 != dist){
+            distance += 1;
+            //printf("dist: %d, distance: %d\n", dist, distance);
+            if(distance == 4) break;
+        }
+        printf("u = %d, dist = %d\n", index_u, distance-1);
+        for(int i = 0; i < 18; ++i){
+            Cube v = neigh(u, i);
+            int index = idx(v);
+            if(arr.get(index) == 3){
+                printf("  node %d found\n", index);
+                arr.set(index, dist);
+                queue.push_back(v);
+            }
+        }
+    }
+    printf("count = %d\n", count-1);
+}
+
+
+
+
 int main(){
-    table arr(554273280);
-    pruning_table(arr);
+    cmb::calcbin();
+    //table arr(554273280);
+    table arr(2217093120);
+    pruning_table2(arr);
+    /*
+    coord::compute_mts();
+    Cube id;
+    coord a(id);
+    coord b = a.neigh(1);
+    a.print();
+    a.neigh(0).print();
+    b.print();
+    //printf("index: %d\n", b.index());
+    */
 }
