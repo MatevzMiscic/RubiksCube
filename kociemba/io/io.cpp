cfad12c16a7f3187283e4749e02c71413b9381c0
MatevzMiscic <matevz.miscic@gmail.com>
2023-09-15 21:13:29 +0200

    Reading and writing of int arrays

--- kociemba/io/io.cpp
+++ kociemba/io/io.cpp
@@ -2,12 +2,28 @@
 
 using namespace std;
 
-int main(){
-    string filename = "data.bin";
-    array<byte, 8> data = {10, 11, 12, 13, 14, 15};
-    io::write(filename, data);
-    io::read(filename, data);
-    for(byte b : data){
-        cout << (int)b << "\n";
+bool io::read(std::string filename, int* data, int n){
+    std::ifstream file(filename, std::ios::in | std::ios::binary);
+    if(!file){
+        return false;
     }
+    file.read((char*)&(data[0]), n);
+    file.close();
+    if(!file.good()){
+        return false;
+    }
+    return true;
+}
+
+bool io::write(std::string filename, int* data, int n){
+    std::ofstream file(filename, std::ios::out | std::ios::binary);
+    if(!file){
+        return false;
+    }
+    file.write((char*)&(data[0]), n);
+    file.close();
+    if(!file.good()){
+        return false;
+    }
+    return true;
 }
--- kociemba/io/io.h
+++ kociemba/io/io.h
@@ -12,20 +12,20 @@ namespace io{
 
     template<size_t n>
     bool read(std::string filename, std::array<byte, n>& data){
-        std::ifstream file(filename, std::ios::out | std::ios::binary);
+        std::ifstream file(filename, std::ios::in | std::ios::binary);
         if(!file){
-        return false;
+            return false;
         }
         file.read((char*)&(data[0]), n);
         file.close();
         if(!file.good()){
-        return false;
+            return false;
         }
         return true;
     }
 
     template<size_t n>
-    int write(std::string filename, std::array<byte, n>& data){
+    bool write(std::string filename, std::array<byte, n>& data){
         std::ofstream file(filename, std::ios::out | std::ios::binary);
         if(!file){
             return false;
@@ -37,5 +37,8 @@ namespace io{
         }
         return true;
     }
+
+    bool read(std::string filename, int* data, int n);
+    bool write(std::string filename, int* data, int n);
 
 }
