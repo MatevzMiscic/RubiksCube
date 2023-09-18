33c344dc3059060742d6e81d2337fc056f491aa7
MatevzMiscic <matevz.miscic@gmail.com>
2023-09-19 01:15:49 +0200

    Implemented ida* algorithm

--- kociemba/solve/solver.cpp
+++ kociemba/solve/solver.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <deque>
+#include <algorithm>
 #include "../preprocess/table.h"
 #include "../moves/moves.h"
 #include "../io/io.h"
@@ -10,10 +11,62 @@ using namespace std;
 
 
 vector<ushort> cornermt;
-vector<uint> layersmt(9676800);
+vector<uint> layermt(9676800);
 
 table corner_arr(40320);
 table layer_arr(967680);
+
+int cornercount = 0;
+int layercount = 0;
+int both = 0;
+
+
+
+
+
+
+uint layer_coord(Cube& cube){
+    std::array<byte, 4> slice;
+    std::array<byte, 8> ud;
+    for(int i = 0; i < 4; ++i) ud[i] = out(cube.edge[i]);
+    for(int i = 0; i < 4; ++i) slice[i] = cube.edge[i + 4] - 4;
+    for(int i = 0; i < 4; ++i) ud[i + 4] = out(cube.edge[i + 8]);
+    return 24 * perm::rank(ud) + perm::rank(slice);
+}
+
+
+
+struct coord{
+    uint layer;
+    ushort corner;
+
+    coord(uint layer, uint corner) : layer(layer), corner(corner){}
+    coord(Cube& cube){
+        layer = layer_coord(cube);
+        corner = cube.corner_coord();
+    }
+
+    inline coord neigh(int i){
+        return coord(layermt[10*layer + i], cornermt[10*corner + i]);
+    }
+
+    bool operator==(const coord& other){
+        return layer == other.layer && corner == other.corner;
+    }
+
+    inline bool is_id(){
+        return layer == 0 && corner == 0;
+    }
+
+    inline void print(){
+        printf("corner: %d, layer: %d\n", corner, layer);
+    }
+};
+
+
+
+
+
 
 
 template<typename type>
@@ -37,39 +90,90 @@ int length(vector<type>& movetable, table& arr, type u){
 }
 
 
-
-uint layer_coord(Cube& cube){
-    std::array<byte, 4> slice;
-    std::array<byte, 8> ud;
-    for(int i = 0; i < 4; ++i) ud[i] = out(cube.edge[i]);
-    for(int i = 0; i < 4; ++i) slice[i] = cube.edge[i + 4] - 4;
-    for(int i = 0; i < 4; ++i) ud[i + 4] = out(cube.edge[i + 8]);
-    return 24 * perm::rank(ud) + perm::rank(slice);
-}
-
-
 int h(Cube& cube){
     int len = length(cornermt, corner_arr, (ushort)cube.corner_coord());
-    int len2 = length(layersmt, layer_arr, layer_coord(cube));
+    int len2 = length(layermt, layer_arr, layer_coord(cube));
     printf("corner len: %d, layer len: %d\n", len, len2);
     if(len2 > len) len = len2;
     return len;
 }
 
 
+inline int h2(uint layer_coord){
+    return length(layermt, layer_arr, layer_coord);
+}
+
+inline int h2(coord& cube){
+    int len1 = length(layermt, layer_arr, cube.layer);
+    int len2 = length(cornermt, corner_arr, cube.corner);
+    if(len2 < len1){
+        layercount += 1;
+        return len1;
+    }else if(len2 == len1){
+        both += 1;
+        return len1;
+    }
+    cornercount += 1;
+    return len2;
+}
+
+
+bool search(vector<coord>& path, int g, int depth){
+    coord u = path.back();
+    if(g + h2(u) > depth) return false;
+    if(u.is_id()) return true;
+    for(int i = 0; i < 10; ++i){
+        coord v = u.neigh(i);
+        if(find(path.begin(), path.end(), v) == path.end()){
+            path.push_back(v);
+            bool found = search(path, g + 1, depth);
+            if(found) return true;
+            path.pop_back();
+        }
+    }
+    return false;
+}
+
+vector<coord> ida(coord u){
+    int depth = h2(u);
+    vector<coord> path = {u};
+    for(int d = depth; d <= 18; ++d){
+        printf("depth = %d\n", d);
+        if(search(path, 0, d)){
+            return path;
+        }
+    }
+    printf("Not found.\n");
+    return path;
+}
+
+
+
+
 int main(){
     cmb::calcbin();
     cornermt = corner_movetable();
-    io::read("../moves/layers.move", (int*)(&layersmt[0]), 4*layersmt.size());
+    io::read("../moves/layers.move", (int*)(&layermt[0]), 4*layermt.size());
     io::read("../preprocess/stage2_corner.bin", corner_arr.bits, (corner_arr.len + 3) / 4);
     io::read("../preprocess/stage2_layer.bin", layer_arr.bits, (layer_arr.len + 3) / 4);
-    std::array<int, 10> index = {2, 3, 6, 7, 8, 9, 10, 11, 14, 15};
-    vector<int> moves = {1, 5, 6, 3, 6, 2, 3, 9, 0, 0, 0, 1, 8, 7};
+    vector<int> index = {2, 3, 6, 7, 8, 9, 10, 11, 14, 15};
     Cube cube;
+    /*
+    vector<int> moves = {1, 5, 6, 3, 6, 2, 3, 9, 0, 0, 0, 1, 8, 7};
     h(cube);
     for(int i : moves){
         cube *= Cube::moves[index[i]];
         h(cube);
     }
+    */
     // there is an error in the corner pruning table
+    
+    for(int i = 0; i < 10; ++i){
+        cube.shuffle(index);
+        vector<coord> path = ida(coord(cube));
+        printf("len: %d\n", path.size());
+    }
+    //for(coord u : path) u.print();
+    
+    printf("corner count: %d, layer count: %d, both: %d\n", cornercount, layercount, both);
 }
