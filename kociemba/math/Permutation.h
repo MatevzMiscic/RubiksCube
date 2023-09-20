da31c35c25fcd2bb24ee44c7fa2d1910bfa86a55
MatevzMiscic <matevz.miscic@gmail.com>
2023-09-21 01:21:44 +0200

    Implmented sign and sign ranking

--- kociemba/math/Permutation.h
+++ kociemba/math/Permutation.h
@@ -5,7 +5,7 @@
 
 
 namespace perm{
-    static std::array<int, 13> fact = {0, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600};
+    static std::array<int, 13> fact = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600};
 
     // returns the rank of the perm[i,...,n-1] on set
     template<size_t n>
@@ -23,6 +23,26 @@ namespace perm{
         Set set(n);
         return rank_helper(perm, 0, set);
     }
+    
+    // returns number of permutations of given sign less than perm[i,..., n-1] on set in lex order
+    template<size_t n>
+    uint rank_sign_helper(const std::array<byte, n>& perm, int i, int sign, Set& set){
+        if(i >= n - 1) return 0;
+        uint a = set.rank(perm[i]);
+        if(i == n - 2) return a * (1 - sign);
+        set.del(perm[i]);
+        uint b = rank_sign_helper(perm, i + 1, (sign ^ a) & 1, set);
+        return a * fact[n - 1 - i] / 2 + b;
+    }
+
+    // returns number of permutations of integers 0, 1,...,n-1 of given sign less than perm in lex order
+    template<size_t n>
+    uint rank_sign(const std::array<byte, n>& perm, int sign){
+        Set set(n);
+        return rank_sign_helper(perm, 0, sign, set);
+    }
+
+
 
     // returns product of permutations a and b where a is applied first
     template<size_t n>
@@ -42,6 +62,26 @@ namespace perm{
         return b;
     }
 
-
+    
+    // returns sign of permutation perm
+    // works for n <= 32
+    template<size_t n>
+    int sign(const std::array<byte, n>& perm){
+        int cycle = 0;
+        int vis = 0;
+        for(int i = 0; i < n; ++i){
+            if((vis & (1 << i)) == 0){
+                cycle += 1;
+                vis |= (1 << i); // for the sake of completness
+                int j = i;
+                while(perm[j] != i){
+                    j = perm[j];
+                    vis |= (1 << j);
+                }
+            }
+        }
+        //printf("   n = %d, cycle = %d    ", n, cycle);
+        return (n - cycle) % 2;
+    }
     
 }
