87a0ba435bb43285862740b360908ed13544aef6
MatevzMiscic <matevz.miscic@gmail.com>
2023-09-24 01:43:03 +0200

    Better solver

--- kociemba/solve/solver.cpp
+++ kociemba/solve/solver.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <string>
+#include <unordered_map>
 #include "../Cube.h"
 #include "../math/Permutation.h"
 #include "../moves/moves.h"
 #include "../io/io.h"
 #include "../Settings.h"
 #include "../preprocess/table.h"
+#include "convert.h"
 
 
 
@@ -38,6 +41,10 @@ struct coord1{
         this->slice = slice;
     }
 
+    inline bool is_solved(){
+        return twist == 0 && flip == 0 && slice == 69;
+    }
+
     inline uint index(){
         return 2187 * ((slice << 11) | flip) + twist;
     }
@@ -52,8 +59,8 @@ struct coord1{
 };
 
 
-vector<int> stage1(Cube& cube){
-    vector<int> moves;
+vector<ubyte> stage1(Cube& cube){
+    vector<ubyte> moves;
     coord1 c(cube);
     bool run = true;
     while(run){
@@ -133,9 +140,9 @@ struct coord2{
     }
 };
 
-vector<int> stage2(Cube& cube){
-    array<int, 10> gens = {2, 3, 6, 7, 8, 9, 10, 11, 14, 15};
-    vector<int> moves;
+vector<ubyte> stage2(Cube& cube){
+    array<ubyte, 10> gens = {2, 3, 6, 7, 8, 9, 10, 11, 14, 15};
+    vector<ubyte> moves;
     coord2 c(cube);
     bool run = true;
     while(run){
@@ -158,13 +165,69 @@ vector<int> stage2(Cube& cube){
 
 
 
-vector<int> solve(Cube cube){
-    vector<int> first = stage1(cube);
+vector<ubyte> solve(Cube cube){
+    vector<ubyte> first = stage1(cube);
     for(int move : first) cube *= Cube::moves[move];
     //cube.print();
-    vector<int> second = stage2(cube);
+    vector<ubyte> second = stage2(cube);
     first.insert(first.end(), second.begin(), second.end());
     return first;
+}
+
+
+
+
+
+
+
+void search(vector<vector<ubyte>>& solutions, vector<ubyte>& moves, coord1& coord, int slack){
+    if(coord.is_solved() && slack == 0){
+        vector<ubyte> copy = moves;
+        solutions.push_back(copy);
+        return;
+    }
+    int dist = stage1_pt.get(coord.index());
+    for(int i = 0; i < 18; ++i){
+        coord1 next = coord.neigh(i);
+        int change = (stage1_pt.get(next.index()) + 4 - dist) % 3;
+        if(change <= slack){
+            moves.push_back(i);
+            search(solutions, moves, next, slack - change);
+            moves.pop_back();
+        }
+    }
+}
+
+vector<vector<ubyte>> all_solutions(Cube& cube, int slack){
+    vector<vector<ubyte>> solutions;
+    vector<ubyte> moves;
+    coord1 coord(cube);
+    search(solutions, moves, coord, slack);
+    return solutions;
+}
+
+vector<ubyte> best_solve(Cube& cube){
+    int final_slack = 0;
+    int len = 31;
+    vector<ubyte> solution;
+    for(int slack = 0; slack < 3; ++slack){
+        vector<vector<ubyte>> sols = all_solutions(cube, slack);
+        for(vector<ubyte>& first : sols){
+            Cube copy = cube;
+            for(byte move : first){
+                copy *= Cube::moves[move];
+            }
+            vector<ubyte> second = stage2(copy);
+            if(first.size() + second.size() < len){
+                len = first.size() + second.size();
+                solution = first;
+                solution.insert(solution.end(), second.begin(), second.end());
+                final_slack = slack;
+            }
+        }
+    }
+    printf("final slack: %d\n", final_slack);
+    return solution;
 }
 
 
@@ -193,7 +256,7 @@ int main(){
 
     printf("tables read: %d\n", read);
 
-
+    /*
     int runs = 50000;
     uint sum = 0;
     uint min = 100000;
@@ -219,6 +282,64 @@ int main(){
     }
     printf("all ok: %d\n", ok);
     printf("avg moves: %f\n", ((double)sum) / runs);
+    printf("max moves: %d\n", max);
+    printf("min moves: %d\n", min);
+    */
+    array<string, 18> names = {
+        "F", "B", "U", "D", "L", "R", 
+        "F2", "B2", "U2", "D2", "L2", "R2", 
+        "F3", "B3", "U3", "D3", "L3", "R3"
+    };
+    array<ubyte, 54> colors1 = {
+        0, 0, 0, 0, 0, 0, 2, 0, 5, 
+        1, 1, 1, 1, 1, 1, 1, 2, 5, 
+        2, 2, 2, 2, 2, 2, 3, 1, 0, 
+        3, 3, 3, 3, 3, 3, 1, 3, 5, 
+        4, 4, 4, 4, 4, 4, 4, 4, 4, 
+        2, 5, 0, 5, 5, 5, 3, 5, 5
+    };
+    array<ubyte, 54> colors = {
+        4, 4, 2, 1, 0, 2, 5, 1, 5,
+        3, 0, 2, 5, 1, 4, 1, 0, 1,
+        5, 4, 0, 3, 2, 5, 0, 3, 0,
+        4, 5, 2, 3, 3, 5, 1, 2, 0,
+        1, 0, 3, 1, 4, 0, 4, 3, 3,
+        2, 1, 4, 2, 5, 2, 3, 4, 5
+    };
+    Cube cube;
+    printf("converted: %d\n", cnv::to_cube(colors, cube));
+    Cube copy = cube;
+    vector<ubyte> moves = best_solve(copy);
+    for(int move : moves){
+        copy *= Cube::moves[move];
+        cout << names[move] << ", ";
+    }
+    copy.print();
+
+    int sum = 0;
+    int min = 100000;
+    int max = 0;
+    int errors = 0;
+    printf("solutions:\n");
+    vector<vector<ubyte>> sols = all_solutions(cube, 1);
+    for(vector<ubyte>& sol : sols){
+        Cube copy = cube;
+        for(byte move : sol){
+            //printf("%d, ", move);
+            copy *= Cube::moves[move];
+        }
+        if(!copy.in_subgroup()){
+            errors += 1;
+        }else{
+            int num = stage2(copy).size();
+            sum += num;
+            if(num > max) max = num;
+            if(num < min) min = num;
+        }
+        //printf("\n");
+    }
+    printf("errors: %d\n", errors);
+    printf("avg moves: %f\n", ((double)sum) / sols.size());
     printf("max moves: %d\n", max);
     printf("min moves: %d\n", min);
 }
